618a9e30e7a1dea302164ad49080a8969f614cbc
Rokiya2932004 <rokiyaabdelsatar293@gmail.com>
2024-06-29 23:51:08 +0300

    add widget iconStyle for app bar

--- lib/views/foldersScreen.dart
+++ lib/views/foldersScreen.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
-
-import '../Component/list_of_Icons.dart';
+import '../widget/IconsStyleforAppBar.dart';
 import '../widget/customAppBar.dart';
 
 class FolderScreen extends StatelessWidget {
@@ -12,17 +11,7 @@ class FolderScreen extends StatelessWidget {
       body:Stack(
         children: [
           CustomAppBar(text: 'folders'),
-          Padding(
-            padding: const EdgeInsets.only(right: 25,top: 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                CustomIcons.addIcon(),
-                CustomIcons.archiveIcon(),
-                CustomIcons.searchIcon(),
-              ],
-            ),
-          )
+          IconsStyle()
 
 
         ],
